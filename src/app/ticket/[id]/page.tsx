3e31e8001e35cc2aeeea029dfb6c42fde0210961
arisevan <115055847+evan9609@users.noreply.github.com>
2023-12-29 02:45:05 +0800

    create

---
+++ src/app/ticket/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+	
+export default function Ticket( { params } : {
+	params:{
+		id: number
+	}
+}) {
+	return (
+		<div>
+			ticket
+		</div>
+	)
+}
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,27 @@
+
+
+import Link from 'next/link'
+import styles from './Nav.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faTicket } from '@fortawesome/free-solid-svg-icons'
+
+const Nav = () => {
+  return (
+    <nav className='flex justify-between p-4 bg-nav'>
+      <div className='flex items-center space-x-4'>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/ticket/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+
+      </div>
+      <div>
+        <p className='text-default-text'>Nav</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
