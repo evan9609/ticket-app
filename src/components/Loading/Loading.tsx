3b9bd86383e7a05a95b8ba89fa012b78cf456165
arisevan <115055847+evan9609@users.noreply.github.com>
2024-01-06 21:42:48 +0800

    loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="fixed w-full h-full bg-slate-700/70">
+      <div className='w-full h-full flex place-content-center place-items-center'>
+        <p className="text-3xl text-blue-600/7">Loading...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
